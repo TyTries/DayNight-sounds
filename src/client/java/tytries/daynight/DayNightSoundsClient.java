a91bb31cfb78c803fc17052f373c66ce4e20ac80
TyTries <ty72064@gmail.com>
2025-05-18 17:32:50 -0700

    Add files via upload

---
+++ src/client/java/tytries/daynight/DayNightSoundsClient.java
@@ -0,0 +1,10 @@
+package tytries.daynight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DayNightSoundsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
